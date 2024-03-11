c374e011ff94ad65072eb97ecab3b5fba2b5b397
Yiğit Tilaver <yigittilaver2000@gmail.com>
2024-03-12 00:05:02 +0100

    added lots of things on the app.tsx and layout.tsx and global.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,137 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import ModeToggle from "@/components/theme-toogle";
+import { Button } from "@/components/ui/button";
+import { beep } from "@/utils/audio";
+import { Separator } from "@/components/ui/separator";
+import { Slider } from "@/components/ui/slider";
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
+import { FlipHorizontal, Camera, Video, Volume2, PersonStanding } from "lucide-react";
+import React, { useRef, useState } from "react";
+import Webcam from "react-webcam";
+import { Rings } from "react-loader-spinner";
+import { toast } from "sonner";
+
+type Props = {};
+
+function Home(props: Props) {
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  // Insanların değiştirebilmesi için
+  const [mirrored, setmirrored] = useState<boolean>(true);
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
+  const [volume, setVolume] = useState<number>(0.1);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex h-screen">
+      {/* // Left-side canvas and webcam */}
+      <div className="relative">
+        <div className="relative h-screen w-full">
+          <Webcam mirrored={mirrored} ref={webcamRef} className="h-full w-full object-contain p-2" />
+          <canvas ref={canvasRef} className="absulute top-h left-0 h-full w-full object-contain "></canvas>
         </div>
       </div>
+      {/* // Right-side buttons and settings */}
+      <div className="flex flex-row flex-1">
+        <div className=" border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4 ">
+          {/* // Top Section */}
+          <div className="flex flex-col gap-2">
+            <Separator />
+            <Button
+              variant="ghost"
+              className="p-2 bg-primary/5 hover:bg-primary/10 rounded-md"
+              onClick={() => {
+                setmirrored((prev) => !prev);
+              }}
+            >
+              <FlipHorizontal />
+            </Button>
+            <ModeToggle />
+          </div>
+          {/* Middle section  */}
+          <div className="flex flex-col gap-2">
+            <Button variant={"outline"} size={"icon"} onClick={userPromptScreenshot}>
+              <Camera />
+            </Button>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+            <Separator className="my-2" />
+
+            <Button variant={isRecording ? "destructive" : "outline"} size={"icon"} onClick={userPromptRecord}>
+              <Video />
+            </Button>
+            <Separator className="my-2" />
+            <Button variant={autoRecordEnabled ? "destructive" : "outline"} size={"icon"} onClick={toggleAutoRecord}>
+              {autoRecordEnabled ? <Rings color="white" height={45} /> : <PersonStanding />}
+            </Button>
+          </div>
+          {/* Bottom Secion  */}
+          <div className="flex flex-col gap-2">
+            <Separator className="my-2" />
+
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant={"ghost"} size={"icon"}>
+                  <Volume2 />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Slider
+                  max={1}
+                  min={0}
+                  step={0.01}
+                  defaultValue={[volume]}
+                  onValueCommit={(val) => {
+                    setVolume(val[0]);
+                    beep(val[0]);
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
+        {/* // for wiki section */}
+        <div className="h-full flex-l py-4 px-2 overflow-y-scroll">
+          <RenderFeatureHighlightsSe1ction />
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
+
+  function userPromptScreenshot() {
+    // take picture
+    //save it to download
+  }
+
+  function userPromptRecord() {
+    if (isRecording) {
+      setIsRecording(false);
+      toast("basma artık çalışmıyorum, görmüyor musun!");
+    } else {
+      setIsRecording(true);
+      toast("şaka yaptım basabilirsin");
+    }
+    // check if recording
+    // then stop redcording
+    // and save to downloads
+    // if not recording
+    // start recording
+  }
+
+  function toggleAutoRecord() {
+    if (autoRecordEnabled) {
+      setAutoRecordEnabled(false);
+      toast("Autorecord is NOT enabled now ");
+    } else {
+      setAutoRecordEnabled(true);
+      toast("Autorecord is enabled now ");
+    }
+  }
 }
+
+function RenderFeatureHighlightsSe1ction() {
+  return <div className="">HEllo Hello</div>;
+}
+
+export default Home;
